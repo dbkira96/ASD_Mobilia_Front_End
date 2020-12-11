204916788c7aecedc013be862a2cbbd5d3e6f901
dbkira96 <dbkira96@gmail.com>
2020-12-11 17:00:44 +0100

    added login response from backend to emulate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
+import { ProductsComponent } from './products/products.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [						
+    AppComponent,
+      WelcomeComponent,
+      LoginComponent,
+      ErrorComponent,
+      ProductsComponent,
+      BoardAdminComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
