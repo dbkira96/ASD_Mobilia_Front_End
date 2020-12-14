d69e0523feba7e95161fc127d99479810f0b1eba
dbkira96 <dbkira96@gmail.com>
2020-12-14 10:53:16 +0100

    first test with material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import{FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import{ MatCardModule}from '@angular/material/card';
+import{MatIconModule} from '@angular/material/icon';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,24 +15,33 @@ import { ErrorComponent } from './error/error.component';
 import { ProductsComponent } from './products/products.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 
 @NgModule({
-  declarations: [						
+  declarations: [							
     AppComponent,
       WelcomeComponent,
       LoginComponent,
       ErrorComponent,
       ProductsComponent,
       BoardAdminComponent,
-      HomePageComponent
+      HomePageComponent,
+      ToolbarComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -9,7 +9,85 @@ export class ProductsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+
+    var product1 = new Product(1,"sedia","arredamento","cucina","A-2",20)
+    var product2 = new Product(2,"tavolo","arredamento","cucina","B-4",30)
+    var product3 = new Product(3,"vaso","arredamento","salotto","C-8",40)
+    var products = [product1,product2,product3];
+    //this.getProductByName(products,"tavolo");
+    //this.getProductsByCategoryAndSubCategory(products,"arredamento","cucina");
+    console.log(product1.quantity);
+    product1.setQuantity(45);
+    console.log(product1.quantity);
+
+    
   }
 
+  getAllProducts(products:Array<Product>): void{
+    for(var i = 0;i<products.length;i++)
+    {
+      console.log(products[i].id);
+      console.log(products[i].name);
+      console.log(products[i].category);
+      console.log(products[i].subcategory);
+      console.log(products[i].quantity);
+    }
+  }
+
+  getProductByName(products:Array<Product>,name:String): void{
+    for(var i = 0;i<products.length;i++)
+    {
+      if(products[i].name == name)
+      {
+      console.log(products[i].quantity);
+      console.log(products[i].scaffale);
+      }
+    }
+  }
+
+  getProductsByCategoryAndSubCategory(products:Array<Product>,category:String,subcategory:String): void{
+    for(var i = 0;i<products.length;i++)
+    {
+      if(products[i].category == category && products[i].subcategory == subcategory)
+      {
+      console.log(products[i].name);
+      }
+    }
+  }
+  
+  
+ }
+
+ class Product {
+  id:number
+  name: string; 
+  category: string; 
+  subcategory: string;
+  scaffale:string;
+  quantity: number; 
+
+  
+  // Default constructor will be called by mapper
+  constructor(id:number,name:string,category:string,subcategory:string,scaffale:string,quantity:number){
+    this.id = id;
+    this.name =name; 
+    this.category =category; 
+    this.subcategory = subcategory; 
+    this.scaffale=scaffale;
+    this.quantity = quantity; 
+  }
+
+  setName(name:string){
+    this.name = name
+  }
+  setScaffale(scaffale:string){
+    this.scaffale = scaffale
+  }
+  setQuantity(quantity:number){
+    this.quantity = quantity
+  }
+
+
+  
 }
